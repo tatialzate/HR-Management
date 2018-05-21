e358bc4fac63f87b5bb1f0072bb20b357a071735
Wendy Tatiana Alzate Cardona <walzate@todo1.com>
2018-05-20 23:38:13 -0500

    Bug fix

--- src/app/employees/employees.module.ts
+++ src/app/employees/employees.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { EmployeesRoutingModule } from './employees-routing.module';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 
